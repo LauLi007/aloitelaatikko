1cb721cfad0c93723f78574d436526529e7c8b09
lauli <lindstedt.laura@gmail.com>
2019-10-02 21:32:24 +0300

    Controller, Entity, Repository ja Migrations tehty

---
+++ src/Controller/AloitelaatikkoController.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Entity\Linkki;
+use App\Form\LinkkiFormType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AloitelaatikkoController extends AbstractController {
+
+    /**
+     * @Route("/aloitelaatikko", name="aloitelaatikko")
+     */
+
+     
+
+}
+
+?>
